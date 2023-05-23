d5d046b4b2751c7c3858444ff1feb25829f86f89
LovIMarK <mark@lovink.ch>
2023-05-23 08:29:02 +0200

    Mouvement Queen + Bishop

--- Bishop.py
+++ Bishop.py
@@ -1,18 +1,33 @@
 import pygame
+from Var import *
 from Piece import Piece
 
 class Bishop(Piece):
 
-    def __init__(self,x,y,size,color,unicode):
-        super().__init__(x,y,size,color)
-        self.unicode_char = unicode
-        self.image = self.font.render(self.unicode_char, True, color)
-        
+    def __init__(self,x,y,size,color,unicode,col,row):
+        super().__init__(x,y,size,color,col,row,"bishop")
+        self.image = self.font.render(unicode, True, color)
+        self.bishopMoves = [(1, 1), (-1, 1), (1, -1), (-1, -1)]
+
         
     
-    def Mouvement(self,posMouse):
-        if self.clicked:
-            self.rect.center=posMouse
+    def Mouvement(self,board):
 
-    def Clicked(self):
-        self.clicked=not self.clicked
+        self.possibleMoves.clear()
+        self.possibleMoves= [[0] * COL for _ in range(ROW)]
+        if self.clicked :
+                
+                for obj in self.bishopMoves:
+                    for i in range(1, 8):
+                        col = self.col + obj[0] * i
+                        row = self.row + obj[1] * i
+                        if 0 <= col < 8 and 0 <= row < 8 and board.Square[col][row].empty:
+                            self.possibleMoves[col][row] = True
+                        elif 0 <= col < 8 and 0 <= row < 8 and not board.Square[col][row].empty :
+                            if board.PiecesPos[col][row]!=0 :
+                                if board.PiecesPos[col][row].color!=self.color:
+                                    self.possibleMoves[col][row] = True
+                                break
+                        else:
+                            break
+
--- Queen.py
+++ Queen.py
@@ -1,19 +1,39 @@
 import pygame
 from Piece import Piece
+from Var import *
+
 
 class Queen(Piece):
 
-    def __init__(self,x,y,size,color,unicode):
-        super().__init__(x,y,size,color)
-        self.unicode_char = unicode
-        self.image = self.font.render(self.unicode_char, True, color)
-        
+    def __init__(self,x,y,size,color,unicode,col,row):
+        super().__init__(x,y,size,color,col,row,"queen")
+        self.image = self.font.render(unicode, True, color)
+        self.queenMoves = [(1, 1), (-1, 1), (1, -1), (-1, -1), (1, 0), (-1, 0), (0, 1), (0, -1)]
+
+
         
     
-    def Mouvement(self,posMouse):
+    def Mouvement(self,board):
+        self.possibleMoves.clear()
+        self.possibleMoves= [[0] * COL for _ in range(ROW)]
         if self.clicked:
-            self.rect.center=posMouse
+            for obj in self.queenMoves:
+                for i in range(1, 8):
+                    col = self.col + obj[0] * i
+                    row = self.row + obj[1] * i
 
-    def Clicked(self):
-        self.clicked=not self.clicked
+                    
+                    if 0 <= col < 8 and 0 <= row < 8 and board.Square[col][row].empty:
+                        self.possibleMoves[col][row] = True
+                    elif 0 <= col < 8 and 0 <= row < 8 and not board.Square[col][row].empty:
+                        if board.PiecesPos[col][row] != 0:
+                            if board.PiecesPos[col][row].color != self.color:
+                                self.possibleMoves[col][row] = True
+                            break
+                    else:
+                        break
+ 
+
+        
+
     
