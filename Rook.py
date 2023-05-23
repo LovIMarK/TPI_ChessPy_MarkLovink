601f685609fb4dda051f6820426218b067a0891d
LovIMarK <mark@lovink.ch>
2023-05-23 08:30:36 +0200

    Optimisation code rook movement

--- Rook.py
+++ Rook.py
@@ -8,49 +8,31 @@ class Rook(Piece):
     def __init__(self,x,y,size,color,unicode,col,row):
         super().__init__(x,y,size,color,col,row,"rook")
         self.image = self.font.render(unicode, True, color)
-        self.rookMoves=[]
+        self.rookMoves=[(0,1),(1,0),(0,-1),(-1,0)]
         
         
-    
-    
-
             
 
     def Mouvement(self,board):
+
+
         self.possibleMoves.clear()
-        self.possibleMoves= [[0] * 8 for _ in range(8)]
-        if self.clicked :
-            
+        self.possibleMoves= [[0] * COL for _ in range(ROW)]
+        if self.clicked:
+            for obj in self.rookMoves:
+                for i in range(1, 8):
+                    col = self.col + obj[0] * i
+                    row = self.row + obj[1] * i
 
-                for col in range(1,COL-self.col):
-                    if board.Square[self.col+col][self.row].empty:
-                        self.possibleMoves[self.col+col][self.row]=True
-                    elif board.PiecesPos[self.col+col][self.row].color!=self.color:
-                        self.possibleMoves[self.col+col][self.row]=True
-                    else :
+                    
+                    if 0 <= col < 8 and 0 <= row < 8 and board.Square[col][row].empty:
+                        self.possibleMoves[col][row] = True
+                    elif 0 <= col < 8 and 0 <= row < 8 and not board.Square[col][row].empty:
+                        if board.PiecesPos[col][row] != 0:
+                            if board.PiecesPos[col][row].color != self.color:
+                                self.possibleMoves[col][row] = True
+                            break
+                    else:
                         break
 
-                for col in range(1,self.col+1):
-                    if board.Square[self.col-col][self.row].empty:
-                        self.possibleMoves[self.col-col][self.row]=True
-                    elif board.PiecesPos[self.col-col][self.row].color!=self.color:
-                        self.possibleMoves[self.col-col][self.row]=True
-                    else :
-                        break
 
-
-                for row in range(1,ROW-self.row):
-                    if board.Square[self.col][self.row+row].empty:
-                        self.possibleMoves[self.col][self.row+row]=True
-                    elif board.PiecesPos[self.col][self.row+row].color!=self.color:
-                        self.possibleMoves[self.col][self.row+row]=True
-                    else :
-                        break
-
-                for row in range(1,self.row+1):
-                    if board.Square[self.col][self.row-row].empty:
-                        self.possibleMoves[self.col][self.row-row]=True
-                    elif board.PiecesPos[self.col][self.row-row].color!=self.color:
-                        self.possibleMoves[self.col][self.row-row]=True
-                    else :
-                        break
